721cbcaea9cf2845067b3b8c0deba64bf48fd162
RithyPhavan <phavanrithy@gmail.com>
2023-08-12 14:05:22 +0700

    add a email and password validator

--- feature/authentication/domain/src/main/java/com/feature/authentication/domain/usecase/LoginUseCase.kt
+++ feature/authentication/domain/src/main/java/com/feature/authentication/domain/usecase/LoginUseCase.kt
@@ -1,16 +1,22 @@
 package com.feature.authentication.domain.usecase
 
 import com.core.common.UiEvent
+import com.core.common.util.validator.EmailValidator
+import com.core.common.util.validator.PasswordValidator
 import com.feature.authentication.domain.repository.AuthRepository
 import com.google.firebase.auth.FirebaseUser
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class LoginUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
     suspend operator fun invoke(email: String, password: String): UiEvent<FirebaseUser> {
-        return authRepository.login(email, password)
+        return if (!EmailValidator.isValidEmail(email)) {
+            UiEvent.Error("Email is invalid.")
+        } else if (!PasswordValidator.isPasswordValid(password)) {
+            UiEvent.Error("Password is invalid.")
+        } else {
+            authRepository.login(email, password)
+        }
     }
 }
